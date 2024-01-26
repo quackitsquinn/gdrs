48cb9b7575c46ac4428cf4f38e835a83428c027f
QuackitsQuinn <quinnweismiller@gmail.com>
2024-01-26 13:33:20 -0600

    small refactors and tests

--- src/saveloading/mod.rs
+++ src/saveloading/mod.rs
@@ -1,5 +1,36 @@
+use self::raw_loader::{decode_save_bytes, SaveDecodeError};
+
 /// This module contains the code for loading a save from a raw byte array.
 /// It is not recommended to use this module directly, as it doesnt account for a mac save or a unencoded save.
 mod raw_loader;
 
 const XOR_SAVE_KEY: u8 = 0x0B;
+
+/// This function takes a save file as a byte array and returns a string. It will attempt to decode the save file if it is encoded.
+pub(crate) fn load_save_str(save_data: Vec<u8>) -> Result<String, SaveDecodeError> {
+    // First we check if the save is encoded or not.
+    if let Ok(save_as_string) = String::from_utf8(save_data.clone()) {
+        return Ok(save_as_string);
+    }
+    // Next, we attempt to decode the save with the window decryption.
+    // TODO: add mac save decryption
+    decode_save_bytes(save_data)
+}
+
+#[cfg(test)]
+mod test {
+    use std::vec;
+
+    use crate::saveloading::load_save_str;
+    /// Tests loading from an actual save file.
+    #[test]
+    fn test_load_save_str() {
+        let bytes = include_bytes!("../../res/CCLocalLevels.dat").to_vec();
+        let decoded = load_save_str(bytes);
+        assert!(
+            decoded.is_ok(),
+            "Failed to decode save bytes {}",
+            decoded.err().unwrap()
+        );
+    }
+}
--- src/saveloading/raw_loader.rs
+++ src/saveloading/raw_loader.rs
@@ -11,7 +11,7 @@ use flate2::bufread::GzDecoder;
 
 /// Loads a save from a raw byte array. This preforms all of the steps to decode the save, but does not do any further processing.
 /// Returns a XML string of the save.
-pub fn decode_save_bytes(bytes: Vec<u8>) -> Result<String, DecodeError> {
+pub fn decode_save_bytes(bytes: Vec<u8>) -> Result<String, SaveDecodeError> {
     // The first step is to xor the bytes with the key.
     let xored_bytes = bytes
         .iter()
@@ -34,7 +34,7 @@ pub fn decode_save_bytes(bytes: Vec<u8>) -> Result<String, DecodeError> {
 }
 /// An error that can occur while decoding a save.
 #[derive(thiserror::Error, Debug)]
-pub enum DecodeError {
+pub enum SaveDecodeError {
     /// The decoder was unable to decode a string from utf8.
     #[error("Failed to decode string from utf8")]
     InvalidString(#[from] std::string::FromUtf8Error),
@@ -44,6 +44,8 @@ pub enum DecodeError {
     /// The decoder was unable to read a string from gzip.
     #[error("Failed to read string from gzip")]
     InvalidGzip(#[from] std::io::Error),
+    #[error("Other error: {0}")]
+    Other(#[from] Box<dyn Error>),
 }
 
 #[cfg(test)]
@@ -59,5 +61,25 @@ mod tests {
             "Failed to decode save bytes {}",
             decoded.err().unwrap()
         );
+    }
+
+    /// Tests loading from a encoded string
+    #[test]
+    fn test_load_save_str_encoded() {
+        const text: &str = "test_load_save_str_encoded";
+        let encrypted: Vec<u8> = vec![
+            0x43, 0x3f, 0x78, 0x42, 0x4a, 0x42, 0x4a, 0x42, 0x7f, 0x4c, 0x5e, 0x4a, 0x54, 0x7c,
+            0x5d, 0x4a, 0x5a, 0x5a, 0x64, 0x4a, 0x42, 0x4a, 0x73, 0x3d, 0x7a, 0x64, 0x71, 0x7b,
+            0x47, 0x59, 0x7a, 0x60, 0x32, 0x5b, 0x3f, 0x59, 0x44, 0x5f, 0x63, 0x5f, 0x63, 0x44,
+            0x78, 0x47, 0x71, 0x64, 0x45, 0x7e, 0x4f, 0x33, 0x5e, 0x4d, 0x54, 0x65, 0x39, 0x4e,
+            0x69, 0x73, 0x64, 0x4a, 0x4a, 0x4a, 0x4a,
+        ];
+        let decoded = decode_save_bytes(encrypted);
+        assert!(
+            decoded.is_ok(),
+            "Failed to decode save bytes {}",
+            decoded.err().unwrap()
+        );
+        assert_eq!(decoded.unwrap(), text);
     }
 }
